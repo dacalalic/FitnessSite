13ce25f09496d35276ae63a733483d5eb49839b4
Danilo <dlalic98@gmail.com>
2020-06-06 00:34:01 +0200

    Almost almost finished

--- script/my_account_sr_script.js
+++ script/my_account_sr_script.js
@@ -5,7 +5,32 @@ for(let i = 0; i < 36; i++){
 let sessAppointment = sessionStorage.getItem("appointment");
 if (sessAppointment) {
     sessAppointment = JSON.parse(sessAppointment);
+    let locA = localStorage.getItem("appointment");
+    locA = JSON.parse(locA);
+
+    let tempDate = new Date();
+    tempDate.setMinutes(tempDate.getMinutes() + 30);
     for(let i = 0; i < sessAppointment.length; i++){
         $("#" + sessAppointment[i]).show();
+        if(locA[sessAppointment[i]].timeDate <= tempDate){
+            $("#" + sessAppointment[i] + " .cancel").hide();
+        }
     }
 }
+
+$(".cancel").on("click", function(){
+    $(this).parent().parent().hide();
+    let id = $(this).parent().parent().attr("id");
+    let locA = localStorage.getItem("appointment");
+    locA = JSON.parse(locA);
+    locA[id].freeNum++;
+    
+    for(let i = 0; i < sessAppointment.length; i++){
+        if(sessAppointment[i] == id){
+            sessAppointment.splice(i, 1);
+        }
+    }
+
+    localStorage.setItem("appointment", JSON.stringify(locA));
+    sessionStorage.setItem("appointment", JSON.stringify(sessAppointment));
+});
